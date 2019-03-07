78116319177cd2000d5113d15f40b1cca4fd2980
Gali Sanjana Reddy <galisanjanareddy@gmail.com>
2019-03-07 20:16:10 +0530

    first commit

---
+++ vendor/bootstrap/js/main.js
@@ -0,0 +1,58 @@
+//js for smoothscroll and Highlight the current section in the navigation bar
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+    
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+      
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 650, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('#navbarResponsive a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('#navbarResponsive ul li a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+}
+
+//reselects the active element after reloading the page
+/*$(document).ready(function () {
+    $(".navbar li a").click(function () {
+        var id = $(this).attr("id");
+
+        $('#' + id).siblings().find(".active").removeClass("active");
+        $('#' + id).addClass("active");
+        localStorage.setItem("selectedolditem", id);
+    });
+
+    var selectedolditem = localStorage.getItem('selectedolditem');
+
+    if (selectedolditem != null) {
+        $('#' + selectedolditem).siblings().find(".active").removeClass("active");
+        $('#' + selectedolditem).addClass("active");
+    }
+});*/
